b52e6960afb651d3142872c6e72ac89cb10bb607
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-10 18:45:34 +0530

    Problem 283

--- Week1/day2.py
+++ Week1/day2.py
@@ -43,3 +43,28 @@ class Solution:
                 res += 1
                 stack.append(n)
         return res
+
+#283 Move zeros to end
+'''Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]'''
+
+class Solution:
+    def movezeros(self ,nums):
+        j=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[j],nums[i]=nums[i],nums[j]
+                j+=1
+        return nums
