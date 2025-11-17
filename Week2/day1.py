756249d02a4d840c1a2e849e23df4423140b58f8
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-17 19:05:24 +0530

    Problem 121 39 ms

--- Week2/day1.py
+++ Week2/day1.py
@@ -77,3 +77,15 @@ class Solution:
             right += 1
         return maxProfit
         
+#Another approach for best time to buy and sell stock
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy=prices[0]
+        profit=0
+        for i in range(1,len(prices)):
+            if prices[i]<buy:
+                buy=prices[i]
+            elif prices[i]-buy>profit:
+                profit=prices[i]-buy
+        return profit
+        
