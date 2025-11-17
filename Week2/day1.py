fab919b27fec26c5d4c355cb5811368b4b300a98
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-17 19:02:20 +0530

    Problem 189 6ms

--- Week2/day1.py
+++ Week2/day1.py
@@ -15,7 +15,7 @@ class Solution:
         return True
 
 
-#   Rotate array
+# 189 Rotate array
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
@@ -30,5 +30,22 @@ class Solution:
         for i in range(n):
             nums[i]=rotated[i]
 __import__("atexit").register(lambda: open("display_runtime.txt", 'w').write('0'))
+
+
+#Another approach for rotate array
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+
+        def reverse(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
 
         
