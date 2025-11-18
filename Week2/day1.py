c2d6e4a2b6bae7e5c6b32423fb0c9d0cd9b4a190
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-18 07:33:21 +0530

    Problem 717

--- Week2/day1.py
+++ Week2/day1.py
@@ -89,3 +89,12 @@ class Solution:
                 profit=prices[i]-buy
         return profit
         
+#717 1 bit and 2 bit characters
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        n=len(bits)
+        i=0
+        while i < n-1:
+            i+= bits[i]+1
+        return i==n-1
+        
