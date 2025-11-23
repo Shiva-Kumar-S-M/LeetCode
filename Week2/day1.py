64b663abebda3c2c63117591ea17f9f67375863a
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-23 09:51:03 +0530

    Problem 1262

--- Week2/day1.py
+++ Week2/day1.py
@@ -165,3 +165,34 @@ class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         return len(list(filter(lambda x: x%3!=0, nums)))
         
+
+#1262 Greatest Sum divisible by three
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+        s = sum(nums)
+        
+        if s % 3 == 0:
+            return s
+        
+        r11 = 10000
+        r12 = 10000
+        r21 = 10000
+        r22 = 10000
+        
+        for num in nums:
+            if num % 3 == 1 and num < r12:
+                if num < r11:
+                    r12 = r11
+                    r11 = num
+                else:
+                    r12 = num
+            if num % 3 == 2 and num < r22:
+                if num < r21:
+                    r22 = r21
+                    r21 = num
+                else: 
+                    r22 = num
+        if s % 3 == 1:
+            return s - min(r11, r21+r22)
+        if s % 3 == 2:
+            return s - min(r21, r11+r12) 
