a9fc88ccf2be12f027611778a9764087ec8cfaa8
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-25 18:10:07 +0530

    Problem easy

--- Week2/day1.py
+++ Week2/day1.py
@@ -213,3 +213,17 @@ class Solution:
             if remainder == 0:
                 return length_N
         return -1
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix_product = 1
+        postfix_product = 1
+        result = [0]*n
+        for i in range(n):
+            result[i] = prefix_product
+            prefix_product *= nums[i]
+        for i in range(n-1,-1,-1):
+            result[i] *= postfix_product
+            postfix_product *= nums[i]
+        return result
