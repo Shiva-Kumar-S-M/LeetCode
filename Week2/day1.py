8c0f7b6844d001f95c5f0b21aefb1f74d58dd935
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-26 19:06:51 +0530

    Hard

--- Week2/day1.py
+++ Week2/day1.py
@@ -312,3 +312,18 @@ class Solution:
                 return True
         return False
     
+#Hard 
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = [n for n in nums if n > 0]
+        nums.sort()
+
+        target = 1
+        for n in nums:
+            if n == target:
+                target += 1
+            elif n > target:
+                return target
+        
+        return target
+    
