4fe7aadab73d56d1798cf2504433e9e74790dd9a
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-27 14:44:02 +0530

    125 another

--- Week2/day1.py
+++ Week2/day1.py
@@ -372,3 +372,22 @@ class Solution:
             left+=1
             right-=1
         return True
+    
+
+#Another Solution for 125 Valid palindrome
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i=0
+        j=len(s)-1
+        s=s.lower()
+
+        while i<=j:
+            while i<j and not s[i].isalnum():
+                i+=1
+            while i<j and not s[j].isalnum():
+                j-=1
+            if s[i]!= s[j]:
+                return False
+            i+=1
+            j-=1
+        return True
