98d877b94cef6685032c58de7beda5fd59244cb7
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-27 16:01:02 +0530

    125 another

--- Week2/day1.py
+++ Week2/day1.py
@@ -391,3 +391,15 @@ class Solution:
             i+=1
             j-=1
         return True
+    
+#14 longest common prefix
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res=""
+
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i==len(s) or s[i]!=strs[0][i]:
+                    return res
+            res+=strs[0][i]
+        return res
