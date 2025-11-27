540203cd83ea6baa7d7e1b5754218899c9f4f242
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-27 05:32:19 +0530

    easy

--- Week2/day1.py
+++ Week2/day1.py
@@ -326,4 +326,20 @@ class Solution:
                 return target
         
         return target
-    
+
+#393 is subsequence
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+        if not t:
+            return False
+        
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        
+        return i == len(s)
