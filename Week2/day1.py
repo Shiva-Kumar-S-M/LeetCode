d9e9643b3aaf00e0e09dd7e90a7239795907a88e
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-29 11:33:12 +0530

    Problem 190

--- Week2/day1.py
+++ Week2/day1.py
@@ -516,3 +516,12 @@ class Solution:
                 offset=i
             dp[i]=1+dp[i-offset]
         return dp
+    
+#190 Reverse bits:
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result=0
+        for i in range(32):
+            bit=(n>>i)&1
+            result|=bit<<(31-i)
+        return result
