c1596fc5bb81fd8339eaac1b074c2585de6ed5c0
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-12-18 18:33:54 +0530

    Leetcode

--- Week2/day1.py
+++ Week2/day1.py
@@ -886,3 +886,16 @@ class Solution:
                 cur.buy=max(cur.buy,  prevP-x)
                 cur.sell=max(cur.sell, prevP+x)
         return dp[-1].profit
+
+class Solution:
+    def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
+        n, k2=len(prices), k//2
+        Sum=list(accumulate((strategy[i]*prices[i] for i in range(n)),initial=0))
+        modify=sum(prices[k2:k])
+        profit=max(Sum[n], modify+Sum[n]-Sum[k])
+
+        for i in range(1, n+1-k):
+            modify+=prices[i+k-1]-prices[i+k2-1]
+            profit=max(profit, modify+Sum[n]-Sum[i+k]+Sum[i])
+        return profit
+        
