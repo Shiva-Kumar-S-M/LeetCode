98bd69037817c1d64b80b09658ba3f11356a3a08
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-12-25 18:02:32 +0530

    Leetcode

--- Week2/day1.py
+++ Week2/day1.py
@@ -1048,3 +1048,17 @@ class Solution:
                 res += 1
 
         return res
+
+class Solution:
+    def maximumHappinessSum(self, christmasJoy: List[int], gifts: int) -> int:
+        # Santa gives gifts to the happiest children first
+        christmasJoy.sort(reverse=True)
+        
+        totalJoy = 0
+        for turn in range(gifts):
+            currentJoy = christmasJoy[turn] - turn
+            if currentJoy <= 0:
+                break
+            totalJoy += currentJoy
+        
+        return totalJoy
