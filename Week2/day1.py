ba30c11f4c954621b328e189b6de6492fff7aa9e
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-01-01 18:06:15 +0530

    Leetcode

--- Week2/day1.py
+++ Week2/day1.py
@@ -1077,3 +1077,14 @@ class Solution:
                 minPenalty = prefix
         
         return bestTime
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n=len(digits)
+        for i in range(n-1,-1,-1):
+            if digits[i] < 9:
+                digits[i]+=1
+                return digits
+            else:
+                digits[i]=0
+        return [1]+digits
+        
