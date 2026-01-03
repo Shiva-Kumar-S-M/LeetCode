3068d88d564273fb49b9d51c6f8c247b70dd23df
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-01-03 09:00:00 +0530

    Best time to buy and sell Stock

---
+++ Arrays/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self,prices):
+        min=float('inf')
+        max=0
+
+        for price in prices:
+            if price<min:
+                min=price
+            
+            profit=price-min
+
+            if profit>max:
+                max=profit
+
+        return max
