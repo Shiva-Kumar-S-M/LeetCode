4ef48174124f086f7386eab5903680cc4aa096b4
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-01-06 21:04:56 +0530

    Leetcode

--- Week2/day1.py
+++ Week2/day1.py
@@ -1149,3 +1149,28 @@ class Solution:
                 minAbs = min(minAbs, av)
 
         return totalSum if neg % 2 == 0 else totalSum - 2 * minAbs
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        idx, Sum=0, -inf
+        q=deque()
+        q.append(root)
+        level=1
+        while q:
+            qz=len(q)
+            curSum=0
+            for i in range(qz):
+                Node=q.popleft()
+                curSum+=Node.val
+                if Node.left: q.append(Node.left)
+                if Node.right: q.append(Node.right)
+            if curSum>Sum:
+                idx, Sum=level, curSum
+            level+=1
+        return idx
