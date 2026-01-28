6873be9714244ebcb463f25db69ed7856320f171
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-01-28 19:20:55 +0530

    Algoo

--- Week3/Algo.py
+++ Week3/Algo.py
@@ -649,3 +649,28 @@ class Solution:
                 j -= 1
 
         return res
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result: List[List[int]] = []
+        nums.sort()  # Sort the array first
+        
+        for i in range(len(nums) - 2):
+            # To avoid duplicates
+            if i == 0 or nums[i] != nums[i - 1]:
+                left, right = i + 1, len(nums) - 1
+                while left < right:
+                    s = nums[i] + nums[left] + nums[right]
+                    if s == 0:
+                        result.append([nums[i], nums[left], nums[right]])
+                        # Skip duplicates for `left` and `right`
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+                    elif s < 0:
+                        left += 1
+                    else:
+                        right -= 1
+        return result
