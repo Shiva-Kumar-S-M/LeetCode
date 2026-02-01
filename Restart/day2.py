b5a4c17ad3942b28af6e033bb22a7704bd73284a
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-02-01 21:23:59 +0530

    Streak

--- Restart/day2.py
+++ Restart/day2.py
@@ -77,3 +77,40 @@ class Solution:
 class Solution:
     def Find_subsequence(self,nums):
         return nums==sorted(nums)
+
+
+class Solution:
+    def minimumCost(self, A: List[int]) -> int:
+        a = 51
+        b = 51
+
+        for i in range(1, len(A)):
+            if A[i] < a:
+                b = a
+                a = A[i]
+            elif A[i] < b:
+                b = A[i]
+
+            if a == 1 and b == 1:
+                break
+
+        return A[0] + a + b
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        # Initialize the first and second variables to the maximum possible value
+        first = float('inf')
+        second = float('inf')
+
+        # Traverse the array
+        for num in nums:
+            # If current number is less than or equal to the first, update first
+            if num <= first:
+                first = num
+            # Else if current number is less than or equal to the second, update second
+            elif num <= second:
+                second = num
+            # Else we found a number greater than both first and second
+            else:
+                return True
+        return False
