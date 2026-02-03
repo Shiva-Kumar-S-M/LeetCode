4a6fec8b3d51478579a7cc82c9021d2bf6f11ca1
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-02-03 18:49:47 +0530

    Streak

--- Restart/Day4.py
+++ Restart/Day4.py
@@ -45,3 +45,22 @@ class solution:
             res.append(i.bit_count())
         return res
     
+
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+        n = len(nums)
+        i = 1
+
+        while i < n and nums[i - 1] < nums[i]:
+            i += 1
+        p = i - 1
+
+        while i < n and nums[i - 1] > nums[i]:
+            i += 1
+        q = i - 1
+
+        while i < n and nums[i - 1] < nums[i]:
+            i += 1
+        flag = i - 1
+
+        return (p != 0) and (q != p) and (flag == n - 1 and flag != q)
