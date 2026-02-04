59c155cc4bf3c1d028105df4fa80d357391e165c
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-02-04 19:08:25 +0530

    Bit and anagram

--- Restart/Day5.py
+++ Restart/Day5.py
@@ -1,4 +1,9 @@
 #Single number 2> 137
+from ast import List
+from collections import defaultdict
+from typing import Tuple
+
+
 class Solution:
     def SingleNumber(self,nums):
         ones,twos=0,0
@@ -43,3 +48,64 @@ class Solution:
             if s.count(i)!=t.count(i):
                 return False
         return True
+    
+#49 group Anagrams
+class Solution:
+    def groupAnagrams(self,strs):
+        res=defaultdict(list)
+
+        for i in strs:
+            key="".join(sorted(i))
+            res[key].append(i)
+        return list(res.values())
+        
+
+#Trionic array  978
+class Solution:
+    def decompose(self, nums: List[int]) -> List[Tuple[int, int, int]]:
+        n = len(nums)
+        subarrays: List[Tuple[int, int, int]] = []
+
+        l = 0
+        s = nums[0]
+
+        for i in range(1, n):
+            # If we fail strict decreasing at boundary i-1 -> i, end the current subarray.
+            if nums[i - 1] <= nums[i]:
+                subarrays.append((l, i - 1, s))
+                l = i
+                s = 0
+            s += nums[i]
+
+        # last subarray
+        subarrays.append((l, n - 1, s))
+        return subarrays
+
+    def maxSumTrionic(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        maxEndingAt = [0] * n
+        for i in range(n):
+            maxEndingAt[i] = nums[i]
+            if i > 0 and nums[i - 1] < nums[i]:
+                if maxEndingAt[i - 1] > 0:
+                    maxEndingAt[i] += maxEndingAt[i - 1]
+
+        maxStartingAt = [0] * n
+        for i in range(n - 1, -1, -1):
+            maxStartingAt[i] = nums[i]
+            if i < n - 1 and nums[i] < nums[i + 1]:
+                if maxStartingAt[i + 1] > 0:
+                    maxStartingAt[i] += maxStartingAt[i + 1]
+
+        PQS = self.decompose(nums)
+        ans = -10**30  
+        for (p, q, s) in PQS:
+            if (p > 0 and nums[p - 1] < nums[p] and
+                q < n - 1 and nums[q] < nums[q + 1] and
+                p < q):
+                cand = maxEndingAt[p - 1] + s + maxStartingAt[q + 1]
+                if cand > ans:
+                    ans = cand
+
+        return ans
