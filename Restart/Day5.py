ef0e1f0edd9a17936666b6d95a02a55d671f50e5
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-02-06 04:46:26 +0530

    212

--- Restart/Day5.py
+++ Restart/Day5.py
@@ -109,3 +109,7 @@ class Solution:
                     ans = cand
 
         return ans
+
+class Solution:
+    def constructTransformedArray(self, A: List[int]) -> List[int]:
+        return [A[(i + v) % len(A)] for i, v in enumerate(A)]
