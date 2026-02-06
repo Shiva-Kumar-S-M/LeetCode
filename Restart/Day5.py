f3327ff4848e74f05fb55fd427918a2be0a2ad43
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-02-06 19:03:30 +0530

    Shortcut

--- Restart/Day5.py
+++ Restart/Day5.py
@@ -115,3 +115,16 @@ class Solution:
         return [A[(i + v) % len(A)] for i, v in enumerate(A)]
     
 print(Solution().constructTransformedArray([0,1,2,4,5,6,7,8,9]))
+
+class Solution:
+    def minRemoval(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        i = 0
+        max_len = 0
+        
+        for j in range(len(nums)):
+            while nums[j] > nums[i] * k:
+                i += 1
+            max_len = max(max_len, j - i + 1)
+            
+        return len(nums) - max_len
