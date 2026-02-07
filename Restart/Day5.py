ed873bdacbb50176771e75703dc59cca7815411e
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-02-07 20:43:25 +0530

    streak

--- Restart/Day5.py
+++ Restart/Day5.py
@@ -128,3 +128,21 @@ class Solution:
             max_len = max(max_len, j - i + 1)
             
         return len(nums) - max_len
+
+
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        res = len(s)
+        a = 0
+        b = 0
+
+        for c in s:
+            a += (c == 'a')
+
+        for c in s:
+            a -= (c == 'a')
+            res = min(res, a + b)
+            b += (c == 'b')
+
+        return res
+
