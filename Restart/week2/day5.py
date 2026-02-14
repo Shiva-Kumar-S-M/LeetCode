db4386bd853b4a4bfbb9a91ba1a5bf21cdaeac30
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-02-14 20:59:15 +0530

    kadene algorithm

--- Restart/week2/day5.py
+++ Restart/week2/day5.py
@@ -151,3 +151,36 @@ class Solution:
             self.duo(s, 'b', 'c'),
             self.trio(s),
         )
+
+
+#20 valid parenthesis easy
+class Solution:
+    def validParenthesis(self,s):
+        stack=[]
+        mapping={')':'(',']':'[','}':'{'}
+
+        for char in s:
+            if char in mapping:
+                topElement=stack.pop() if stack else '#'
+
+                if mapping[char]!=topElement:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
+
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        tower = [[0] * 102 for _ in range(102)]
+        tower[0][0] = poured
+        
+        for r in range(query_row + 1):
+            for c in range(r + 1):
+                if tower[r][c] > 1:
+                    excess = (tower[r][c] - 1.0) / 2.0
+                    tower[r][c] = 1
+                    tower[r+1][c] += excess
+                    tower[r+1][c+1] += excess
+                    
+        return tower[query_row][query_glass]
