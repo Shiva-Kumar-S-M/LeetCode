ec3b55c27e3536543ab8761c22103d417811fe5b
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-02-19 19:03:29 +0530

    push

--- Restart/week2/week3/day3.py
+++ Restart/week2/week3/day3.py
@@ -80,3 +80,20 @@ class Solution:
     def readBinaryWatch(self,n):
         x=x^(x>>1)
         return x&(x+1)==0
+
+
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        res = 0
+        prev = 0
+        strk = 1
+
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]: strk += 1
+            else:
+                prev = strk
+                strk = 1
+
+            if strk <= prev: res += 1
+
+        return res
