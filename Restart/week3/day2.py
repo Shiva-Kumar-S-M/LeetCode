9bc90db4f0d95efc70fb44a21236da6559bac57f
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-02-26 18:46:35 +0530

    Bit manipulation

--- Restart/week3/day2.py
+++ Restart/week3/day2.py
@@ -41,3 +41,15 @@ class Solution:
 class Solution:
     def sortByBits(self, arr):
         return sorted(arr, key=lambda x: (x.bit_count(), x))
+
+
+class Solution:
+    def numSteps(self, s: str) -> int:
+        steps = 0
+        carry = 0
+        for i in range(len(s) - 1, 0, -1):
+            bit = ord(s[i]) & 1
+            steps += 1 + (bit ^ carry)
+            carry |= bit
+
+        return steps + carry
