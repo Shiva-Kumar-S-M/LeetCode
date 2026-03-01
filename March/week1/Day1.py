37ad73e2b7b57dd76a83f143ccc59c8a8de6e290
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-03-01 16:53:09 +0530

    Rotate array(189)

--- March/week1/Day1.py
+++ March/week1/Day1.py
@@ -16,3 +16,26 @@ class Solution:
 
         return list
         
+
+
+#Rotate array(189)Medium
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        k%=n
+
+        def rev(l,r):
+            while l<r:
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+                r-=1
+
+        rev(0,n-1)
+        rev(0,k-1)
+        rev(k,n-1)
+
+        
