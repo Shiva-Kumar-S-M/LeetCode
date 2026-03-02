f83494ab940d210a759fdd089d2308879fa6e8d3
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-03-02 19:32:05 +0530

    Buy & sell Stock

--- March/week1/Day2.py
+++ March/week1/Day2.py
@@ -15,3 +15,15 @@ class Solution:
                 max=profit
 
         return profit
+    
+
+#122 Best time to buy and sell stock II medium
+class Solution:
+    def maxProfit(self,prices):
+        max=0
+
+        for i in range(1,len(prices)):
+            if prices[i]>prices[i-1]:
+                max+=prices[i]-prices[i-1]
+
+        return max
