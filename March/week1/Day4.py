8528c0d1aead6a1867fb31f3c9b0f3c1c9c18f8b
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-03-05 19:22:56 +0530

    Streak

--- March/week1/Day4.py
+++ March/week1/Day4.py
@@ -58,3 +58,11 @@ class Solution:
             for i in range(m)
             for j in range(n)
         )
+    
+class Solution:
+    def minOperations(self, s: str) -> int:
+        count, n = 0, len(s)
+        for i in range(n):
+            count += (ord(s[i]) ^ i) & 1
+            
+        return min(count, n - count)
