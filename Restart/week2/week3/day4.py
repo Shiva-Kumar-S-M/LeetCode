c986e7947b95b302ccc6f21fd872a353563dbf8c
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-03-11 18:50:31 +0530

    Streak

--- Restart/week2/week3/day4.py
+++ Restart/week2/week3/day4.py
@@ -160,3 +160,12 @@ class Solution:
             prv, cur, nxt = cur, nxt, ways(one, k + 2)
 
         return result % MOD
+
+
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        if n == 0: return 1
+        mask = n
+        for i in (1, 2, 4, 8, 16):
+            mask |= mask >> i
+        return ~n & mask
