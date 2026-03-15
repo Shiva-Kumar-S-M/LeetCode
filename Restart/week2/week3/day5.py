c3d408d5cb0aaef7b1311380d8084cc71d1b3b15
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-03-15 21:18:45 +0530

    Tree invertor

--- Restart/week2/week3/day5.py
+++ Restart/week2/week3/day5.py
@@ -122,3 +122,18 @@ class Solution:
 
         recursion(None, root, False)
         return res.values()
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    
+        if not root:
+            return
+        
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
