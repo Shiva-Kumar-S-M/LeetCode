cba0e6f8e1f58f260b2a0037a1c3dd66887d1ffe
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-03-25 05:11:24 +0530

    Streak

--- Binary Search/daily.py
+++ Binary Search/daily.py
@@ -163,3 +163,23 @@ class Solution:
                 dp[i][j][0], dp[i][j][1]=min(xx), max(xx)
         ans=dp[r-1][c-1][1]
         return -1 if ans<0 else ans%MOD
+
+class Solution:
+    def constructProductMatrix(self, grid):
+        MOD = 12345
+        n, m = len(grid), len(grid[0])
+        p = [[0]*m for _ in range(n)]
+
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):
+                p[i][j] = suffix
+                suffix = (suffix * (grid[i][j] % MOD)) % MOD
+
+        prefix = 1
+        for i in range(n):
+            for j in range(m):
+                p[i][j] = (p[i][j] * prefix) % MOD
+                prefix = (prefix * (grid[i][j] % MOD)) % MOD
+
+        return p
