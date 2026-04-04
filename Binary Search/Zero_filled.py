2086839832e5db3cf01395628a905533a88810d4
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-04-04 21:16:08 +0530

    Medium

--- Binary Search/Zero_filled.py
+++ Binary Search/Zero_filled.py
@@ -263,3 +263,22 @@ class Solution:
                         break
 
         return [h[i] for i in range(n) if alive[i]]
+
+
+class Solution:
+    def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+        if rows == 1:
+            return encodedText
+
+        n = len(encodedText)
+        cols = n // rows
+        res = []
+
+        for c in range(cols):
+            r, j = 0, c
+            while r < rows and j < cols:
+                res.append(encodedText[r * cols + j])
+                r += 1
+                j += 1
+
+        return "".join(res).rstrip()
