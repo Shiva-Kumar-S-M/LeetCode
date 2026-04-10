1a5e7f79fcc3d81e69f48b98e85ed22de2ac7407
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-04-10 21:48:41 +0530

    'Easy'

--- Binary Search/Zero_filled.py
+++ Binary Search/Zero_filled.py
@@ -460,3 +460,22 @@ class Solution:
             ans ^= num
             
         return ans
+
+
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        n = len(nums)
+        last2 = [0] * n
+        res = 200
+
+        for i in range(n):
+            val, pos = nums[i] - 1, i + 1
+            pack = last2[val]
+            old, cur = pack & 255, pack >> 8
+
+            last2[val] = cur | (pos << 8)
+
+            if old:
+                res = min(res, (pos - old) << 1)
+
+        return -(res == 200) | res
