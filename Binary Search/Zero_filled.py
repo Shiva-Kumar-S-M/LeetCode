fafb88a194505b05143cf79650f0ce7daa1a8f93
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-04-10 21:51:09 +0530

    Clearing Error

--- Binary Search/Zero_filled.py
+++ Binary Search/Zero_filled.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import math
 from typing import List
 
 
@@ -387,30 +388,30 @@ class Robot:
 
 
     class Solution:
-    def xorAfterQueries(self, nums, queries):
-        mod = 1000000007
+        def xorAfterQueries(self, nums, queries):
+            mod = 1000000007
 
         # Process each query
-        for t in queries:
-            l = t[0]
-            r = t[1]
-            k = t[2]
-            v = t[3]
+            for t in queries:
+                l = t[0]
+                r = t[1]
+                k = t[2]
+                v = t[3]
 
-            idx = l
+                idx = l
 
             # Apply operation at step k
-            while idx <= r:
-                temp = nums[idx]
-                nums[idx] = (temp * v) % mod
-                idx += k
+                while idx <= r:
+                    temp = nums[idx]
+                    nums[idx] = (temp * v) % mod
+                    idx += k
 
         # Compute XOR of final array
-        ans = 0
-        for num in nums:
-            ans ^= num
+            ans = 0
+            for num in nums:
+                ans ^= num
 
-        return ans
+            return ans
 
 class Solution:
     def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
