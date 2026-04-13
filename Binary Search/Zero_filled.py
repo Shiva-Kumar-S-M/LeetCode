fae50124e808b684f317867093720c338d92f76c
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-04-13 19:46:11 +0530

    Streak

--- Binary Search/Zero_filled.py
+++ Binary Search/Zero_filled.py
@@ -513,4 +513,20 @@ class Solution:
             prev=x
         return min(dp[-1])
         
-            
+class Solution:
+    def getMinDistance(self, nums: list[int], target: int, start: int) -> int:
+
+        if nums[start] == target:
+            return 0
+
+        n = len(nums)
+        d = 1
+
+        while True:
+            if start - d >= 0 and nums[start - d] == target:
+                return d
+
+            if start + d < n and nums[start + d] == target:
+                return d
+
+            d += 1    
