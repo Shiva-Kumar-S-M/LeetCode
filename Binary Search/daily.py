92ff51574fc4a314418eea370084b36a66c713fb
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-04-17 19:12:46 +0530

    Medium

--- Binary Search/daily.py
+++ Binary Search/daily.py
@@ -239,3 +239,15 @@ class Solution:
             ans.append(res)
 
         return ans
+
+class Solution:
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
+        res = 100000
+        seen = {}
+
+        for i, n in enumerate(nums):
+            if n in seen:
+                res = min(res, i - seen[n])
+            seen[int(str(n)[::-1])] = i
+
+        return -(res == 100000) | res
