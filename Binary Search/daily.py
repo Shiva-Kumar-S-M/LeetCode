6d239664e608948fb666f2d8a8c91091c12f2d10
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-04-18 22:12:53 +0530

    Easy

--- Binary Search/daily.py
+++ Binary Search/daily.py
@@ -251,3 +251,12 @@ class Solution:
             seen[int(str(n)[::-1])] = i
 
         return -(res == 100000) | res
+
+class Solution:
+    def mirrorDistance(self, n: int) -> int:
+        rev, x=0, n
+        while x>0:
+            x, r=divmod(x, 10)
+            rev=10*rev+r
+        return abs(rev-n)
+        
