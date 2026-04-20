2a721249a55a1fc493d76c08908093e03c8689c1
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-04-20 22:09:13 +0530

    Discipline

--- Binary Search/Zero_filled.py
+++ Binary Search/Zero_filled.py
@@ -569,3 +569,21 @@ class Solution:
             j += 1
 
         return j - i - 1
+    
+
+class Solution:
+    def maxDistance(self, A: List[int]) -> int:
+        n = len(A)
+        left, right = 0, n - 1
+
+        for i in range(n):
+            if A[i] ^ A[-1]:
+                left = i
+                break
+
+        for i in range(n - 1, -1, -1):
+            if A[i] ^ A[0]:
+                right = i
+                break
+
+        return max(n - 1 - left, right)
