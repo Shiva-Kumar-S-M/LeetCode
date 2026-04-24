3f0e0dc5586e589e9fcdfb0f5570a5979799c64d
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-04-24 23:34:06 +0530

    Neha

--- Binary Search/Zero_filled.py
+++ Binary Search/Zero_filled.py
@@ -756,4 +756,13 @@ class Solution:
                 left_sum += pos[i]
 
         return ans
+
+
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        x, r=0, 0
+        for c in moves:
+            x+=(c=='R')-(c=='L')
+            r+=c=='_'
+        return abs(x)+r
         
