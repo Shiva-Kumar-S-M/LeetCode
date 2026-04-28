87abcc4043c9844bb9c87cc97a6126e35f0a9677
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-04-28 22:47:31 +0530

    Added

--- Binary Search/Zero_filled.py
+++ Binary Search/Zero_filled.py
@@ -924,3 +924,14 @@ class Solution:
             ops += abs(i - median) // x * freq[i]
 
         return ops
+
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        nums.sort()
+        median = nums[len(nums) // 2]
+
+        moves = 0
+        for num in nums:
+            moves += abs(num - median)
+
+        return moves
