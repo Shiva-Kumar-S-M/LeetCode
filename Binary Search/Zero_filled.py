c1b7dbbb2fa8affbb19c7ea4344757c0fea7ac56
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-04-29 22:36:26 +0530

    Medium

--- Binary Search/Zero_filled.py
+++ Binary Search/Zero_filled.py
@@ -1007,3 +1007,19 @@ class Solution:
             ans = max(ans, dp[0][k], dp[n][k])
 
         return ans
+
+class Solution:
+    def maxScore(self, grid: List[List[int]]) -> int:
+
+        ans, m, n = -inf, len(grid), len(grid[0])
+        grid = [[inf]*n] + grid
+        grid = [[inf]+row for row in grid]
+        # for row in grid: print(row)
+        
+        for row, col in product(range(m), range(n)):
+            mn = min(grid[row][col+1], grid[row+1][col])
+            ans = max(ans, grid[row+1][col+1] - mn)
+            grid[row+1][col+1] = min(grid[row+1][col+1], mn)
+         # for row in grid: print(row)
+        
+        return ans
