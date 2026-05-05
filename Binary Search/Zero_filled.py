beb7d6782049cb93676858d806a70e4e3ef8caf6
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-05-05 22:09:16 +0530

    Trying

--- Binary Search/Zero_filled.py
+++ Binary Search/Zero_filled.py
@@ -1108,3 +1108,34 @@ class Solution:
             for j in range(i, n - 1 - i):
                 mat[i][j], mat[j][~i], mat[~i][~j], mat[~j][i] = \
                 mat[~j][i], mat[i][j], mat[j][~i], mat[~i][~j]
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        if not head:
+            return None
+
+        cur=head
+        length=1
+
+        while cur.next:
+            cur=cur.next
+            length+=1
+        
+        cur.next=head
+
+        k=length-(k%length)
+
+        while k>0:
+            cur=cur.next
+            k-=1
+        
+        newhead=cur.next
+        cur.next=None
+        
+        return newhead
