6001c6eea045dd3ac3bfe1617e4b282138688477
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-05-16 21:30:45 +0530

    Streak

--- Binary Search/Koko_eating.py
+++ Binary Search/Koko_eating.py
@@ -222,3 +222,11 @@ class Solution:
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         return nums[bisect_left(nums, True, key=lambda n: n <= nums[-1])]
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        while len(nums) > 1 and nums[-1] == nums[0]:
+            nums.pop()
+
+        return nums[bisect_left(nums, True, key=lambda n: n <= nums[-1])]
