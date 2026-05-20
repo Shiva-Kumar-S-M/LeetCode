9498a09d5cb1c22754ca7bf41f4e343819fbbe7f
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-05-20 22:25:28 +0530

    ujiuefnjeinfji

--- Binary Search/Koko_eating.py
+++ Binary Search/Koko_eating.py
@@ -328,3 +328,21 @@ class Solution:
                 queue2.popleft()  # Erode the smaller value
                 
         return -1
+
+
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        n = len(A)
+        res = [0] * n
+        seen = [0] * (n + 1)
+        
+        for i in range(n):
+            seen[0] += seen[A[i]]
+            seen[A[i]] = 1
+            
+            seen[0] += seen[B[i]]
+            seen[B[i]] = 1
+            
+            res[i] = seen[0]
+            
+        return res
