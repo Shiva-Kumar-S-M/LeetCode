6b7ba327e2ed7e1ca46af8783ccd4002d50c3cff
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-05-23 22:25:00 +0530

    jsjsjsjdnjdsj

--- Binary Search/Zero_filled.py
+++ Binary Search/Zero_filled.py
@@ -1210,3 +1210,7 @@ class Solution:
             l = r + 1
 
         return ans
+
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        return sum(nums[i - 1] > nums[i] for i in range(len(nums))) < 2
