8a248759897f73732e5e1c4430d64a7c8a9aef09
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-05-25 22:21:22 +0530

    Trying

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -19,3 +19,27 @@ class Solution:
                 end = mid - 1
 
         return mid + 1 if target > nums[mid] else mid
+
+class Solution:
+    def canReach(self, s: str, minJ: int, maxJ: int) -> bool:
+        n = len(s)
+
+        if int(s[-1]): return False
+
+        dp = [False] * n
+        dp[0] = True
+        reach, maxR = 0, maxJ
+
+        for i in range(minJ, n):
+            if i > maxR: return False
+
+            reach += dp[i - minJ]
+
+            if i > maxJ:
+                reach -= dp[i - maxJ - 1]
+
+            if reach and not int(s[i]):
+                dp[i] = True
+                maxR = i + maxJ
+
+        return reach > 0
