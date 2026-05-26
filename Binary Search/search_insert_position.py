3f1277aee11a6f8f0369bd846c8c7111bbe92b05
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-05-26 22:36:04 +0530

    ndbhfhs

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -43,3 +43,19 @@ class Solution:
                 maxR = i + maxJ
 
         return reach > 0
+
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        lower = 0
+        upper = 0
+
+        for ch in word:
+            if ch.islower():
+                lower |= (1 << (ord(ch) - ord('a')))
+            else:
+                upper |= (1 << (ord(ch) - ord('A')))
+
+        common = lower & upper
+
+        # counting number of set bits
+        return common.bit_count()
