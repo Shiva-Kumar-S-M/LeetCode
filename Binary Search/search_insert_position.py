7651c271ca084e03ed4e94041e40be505c01f873
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-06-01 23:09:02 +0530

    nccjsdhddhj

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -235,4 +235,20 @@ class Solution:
                 return False
             planet+=x*f
         return True
-        
+
+
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        cost.sort()
+        total = 0
+
+        i = len(cost) - 1
+
+        while i >= 0:
+            total += cost[i]
+            if i - 1 >= 0:
+                total += cost[i - 1]
+
+            i -= 3
+
+        return total
