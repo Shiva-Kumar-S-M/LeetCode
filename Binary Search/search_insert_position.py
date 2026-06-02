c13658226721b9d375963d679cd1599cf0ccc7bd
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-06-02 16:36:09 +0530

    dnks

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -252,3 +252,28 @@ class Solution:
             i -= 3
 
         return total
+
+
+class Solution:
+    def calFinishTime(self, ls, ld, ws, wd):
+
+        mini = float('inf')
+
+        for i in range(len(ls)):
+            mini = min(mini, ls[i] + ld[i])
+
+        ans = float('inf')
+
+        for i in range(len(ws)):
+            ans = min(
+                ans,
+                max(mini, ws[i]) + wd[i]
+            )
+
+        return ans
+
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        return min(
+            self.calFinishTime(landStartTime,landDuration,waterStartTime,waterDuration),
+            self.calFinishTime(waterStartTime,waterDuration,landStartTime,landDuration)
+        )
