987fdd3eba1318835e08d099cb173d37082fe8ea
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-06-06 21:03:10 +0530

    Room shifitng

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -355,3 +355,7 @@ class Solution:
             count += ways * mult
 
         return count
+
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        return (L:=list(accumulate(nums, initial=0))) and [abs(L[-1]-x-2*l) for l, x in zip(L, nums)]
