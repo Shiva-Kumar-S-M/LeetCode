4b8b71fab8b8a42b602180c4b8c283af6e4bf7b3
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-06-07 23:48:29 +0530

    kdmks

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -359,3 +359,23 @@ class Solution:
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
         return (L:=list(accumulate(nums, initial=0))) and [abs(L[-1]-x-2*l) for l, x in zip(L, nums)]
+
+class Solution:
+    def createBinaryTree(self, A: List[List[int]]) -> Optional[TreeNode]:
+        nodes = {}
+        root = 0
+
+        for x, y, is_left in A:
+            if x not in nodes:
+                nodes[x] = TreeNode(x)
+                root ^= x
+            if y not in nodes:
+                nodes[y] = TreeNode(y)
+                root ^= y
+            if is_left:
+                nodes[x].left = nodes[y]
+            else:
+                nodes[x].right = nodes[y]
+            root ^= y
+
+        return nodes[root]
