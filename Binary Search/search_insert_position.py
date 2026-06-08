24eef93ae9de841f847e24f9b20c739b104e14fa
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-06-08 22:22:59 +0530

    jndj

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -379,3 +379,12 @@ class Solution:
             root ^= y
 
         return nodes[root]
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        return (
+            [n for n in nums if n < pivot] +
+            [n for n in nums if n == pivot] +
+            [n for n in nums if n > pivot]
+        )
+        
