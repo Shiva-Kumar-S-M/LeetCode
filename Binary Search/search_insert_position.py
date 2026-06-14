3b0911a65a4ad18b2fb1b2455d57e850ce8409ba
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-06-14 21:02:35 +0530

    445

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -551,3 +551,19 @@ class Solution:
             res.append(chr(122 - (s - ((s * 2521) >> (1 << 4)) * len(wt))))
 
         return "".join(res)
+
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        slow = fast = head
+        prev = None
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow.next, prev, slow = prev, slow, slow.next
+
+        res = 0
+        while slow:
+            res = max(res, prev.val + slow.val)
+            prev, slow = prev.next, slow.next
+
+        return res
