6d609050154284902a096d6de13d0e86b2a214e6
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-06-15 21:42:37 +0530

    nhjd

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -567,3 +567,17 @@ class Solution:
             prev, slow = prev.next, slow.next
 
         return res
+
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head.next: return None
+
+        slow = head
+        fast = slow.next.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        slow.next = slow.next.next
+        return head
