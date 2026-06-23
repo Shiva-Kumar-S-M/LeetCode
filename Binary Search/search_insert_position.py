359b511b0c5579a93fbc5b2f5d3d7861009fd475
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-06-23 22:32:54 +0530

    njnjds

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -732,3 +732,18 @@ class Solution:
             maxBalloons = min(maxBalloons, freqMap.get(key, 0) // count)
 
         return 0 if maxBalloons == float('inf') else maxBalloons
+
+
+class Solution:
+    def zigZagArrays(self, n: int, l: int, r: int) -> int:
+        MOD = 1000000007
+        m = r - l + 1
+        dp = [1] * m
+
+        for i in range(2, n + 1):
+            dp.reverse()
+            s = 0
+            for j in range(m):
+                dp[j], s = s, (s + dp[j]) % MOD
+
+        return (sum(dp) % MOD << 1) % MOD
