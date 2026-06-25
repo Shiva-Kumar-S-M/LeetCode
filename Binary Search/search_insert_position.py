dc9dfd353403fadc7a8c209dbd899af46cf21fff
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-06-25 21:40:18 +0530

    bdhbdhs

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -804,3 +804,23 @@ class Solution:
             answer = (answer + row_sum) % MOD
 
         return answer
+
+
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        ans = 0
+
+        for l in range(n):
+            target_count = 0
+
+            for r in range(l, n):
+                if nums[r] == target:
+                    target_count += 1
+
+                length = r - l + 1
+
+                if target_count > length // 2:
+                    ans += 1
+
+        return ans
