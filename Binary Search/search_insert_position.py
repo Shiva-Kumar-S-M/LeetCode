9d874c81ccfc792166314fc51410f2c457070f57
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-07-07 21:19:12 +0530

    fnj

--- Binary Search/search_insert_position.py
+++ Binary Search/search_insert_position.py
@@ -1124,3 +1124,9 @@ class Solution:
             r = max(r, end)
 
         return res
+
+class Solution:
+    def sumAndMultiply(self, n: int) -> int:
+        digits = [int(c) for c in str(n) if c != '0']
+        x = int(''.join(map(str, digits))) if digits else 0
+        return x * sum(digits)
