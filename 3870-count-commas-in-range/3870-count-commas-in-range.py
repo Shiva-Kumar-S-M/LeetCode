e99c596349d2a62aef9fa33d84dc7f6aa5cca33b
Shiva Kumar S.M <shivukumar.iu@gmail.com>
2026-09-25 12:12:11 +0530

    chore: remove old root-level duplicate files (batch 2/2)

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countCommas(self, n: int) -> int:
-        count = 0
-
-        for i in range(1, n + 1):
-            if i >= 1000:
-                count += 1
-
-        return count
--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def uniformArray(self, A: list[int]) -> bool:
-        return not (min(A) ^ reduce(or_, A)) & 1
